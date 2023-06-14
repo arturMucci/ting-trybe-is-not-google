09f08f0896f6199c868cf5e4eeaef3584163b333
arturMucci <artur.mucci@gmail.com>
2023-06-13 21:25:01 -0300

    ProjetoTing - milestone 01

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,25 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        """Inicializa a fila"""
+        self._queue = []
 
     def __len__(self):
         """Aqui irá sua implementação"""
+        return len(self._queue)
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
+        return self._queue.append(value)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
+        if len(self._queue) == 0:
+            return None
+        return self._queue.pop(0)
 
     def search(self, index):
         """Aqui irá sua implementação"""
+        if index < 0 or index >= len(self._queue):
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self._queue[index]
