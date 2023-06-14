aa78dd0b0a5eec0c650a17134ac67bee8950d1f3
arturMucci <artur.mucci@gmail.com>
2023-06-14 08:43:52 -0300

    ProjetoTing - milestone 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    try:
+        if path_file == "" or not path_file.endswith(".txt"):
+            sys.stderr.write("Formato inválido")
+        with open(path_file, "r") as f:
+            return [line.rstrip() for line in f]
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
